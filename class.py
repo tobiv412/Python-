cf6bca0dda069c789d7905038cca07cb8de12453
tobiv412 <victor.tobi@cloudreach.com>
2021-11-13 13:54:08 +0000

    class python

---
+++ class.py
@@ -0,0 +1,20 @@
+#!/Users/victor.tobi/.pyenv/shims/python
+
+class Car:
+    def __init__(self, color):
+        self.color = color
+    
+    def set_color(self, color):
+        self.color = color
+
+
+my_first_car = Car("red")
+my_second_car = Car("blue")
+
+print(my_first_car.color)
+
+print(my_second_car.color)
+
+my_first_car.set_color("Purple")
+
+print(my_first_car.color)
